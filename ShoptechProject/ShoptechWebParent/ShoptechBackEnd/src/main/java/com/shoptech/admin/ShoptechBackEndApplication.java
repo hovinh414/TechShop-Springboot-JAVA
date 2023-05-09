778e63024205b37168602d7ca42ed36165cd5c1b
PhanHongYenQuynh <luisywang1411@gmail.com>
2023-05-09 09:01:32 +0700

    Update Entity User, User Repository, User Test

--- ShoptechProject/ShoptechWebParent/ShoptechBackEnd/src/main/java/com/shoptech/admin/ShoptechBackEndApplication.java
+++ ShoptechProject/ShoptechWebParent/ShoptechBackEnd/src/main/java/com/shoptech/admin/ShoptechBackEndApplication.java
@@ -1,9 +1,12 @@
 package com.shoptech.admin;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages={"com.shoptech.site.entity", "com.shoptech.admin.user"})
 public class ShoptechBackEndApplication {
 
 	public static void main(String[] args) {
