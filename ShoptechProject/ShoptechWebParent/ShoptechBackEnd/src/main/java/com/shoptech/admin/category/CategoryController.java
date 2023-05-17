e671cb92a158059bba7214f1949e4515fffda810
hovinh414 <hovinh414@gmail.com>
2023-05-18 01:04:12 +0700

    update sort asc and desc category by Zinh

--- ShoptechProject/ShoptechWebParent/ShoptechBackEnd/src/main/java/com/shoptech/admin/category/CategoryController.java
+++ ShoptechProject/ShoptechWebParent/ShoptechBackEnd/src/main/java/com/shoptech/admin/category/CategoryController.java
@@ -3,13 +3,11 @@ package com.shoptech.admin.category;
 import com.shoptech.admin.upload.FileUploadUtil;
 import com.shoptech.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -20,34 +18,44 @@ public class CategoryController {
     @Autowired
     private CategoryService service;
     @GetMapping("/categories")
-    public String listAll(Model model){
-        List<Category> listCategories = service.listAll();
+    public String listFirstPage(@Param("sortDir") String sortDir, Model model){
+
+        if (sortDir == null || sortDir.isEmpty()){
+            sortDir = "asc";
+
+        }
+        List<Category> listCategories = service.listAll(sortDir);
+        String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
         model.addAttribute("listCategories",listCategories);
+        model.addAttribute("reverseSortDir", reverseSortDir);
         return "categories/categories";
     }
-    @GetMapping("/categoríes/create")
+    @GetMapping("/categories/create")
     public String newCategory(Model model){
         List<Category> listCategories = service.listCategoriesUsedInForm();
         model.addAttribute("category", new Category());
         model.addAttribute("listCategories", listCategories);
-        model.addAttribute("pageTitle", "Create new Category");
+        model.addAttribute("pageTitle", "Thêm mới danh mục");
         return "categories/category_form";
     }
     @PostMapping("/categories/save")
-    public String saveCategory(Category category, @RequestParam("fileImage") MultipartFile multipartFile,
-                               RedirectAttributes ra) throws IOException {
-        if (!multipartFile.isEmpty()){
+    public String saveCategory(@ModelAttribute("category") Category category, RedirectAttributes redirectAttributes,
+                               @RequestParam("fileImage") MultipartFile multipartFile) throws IOException {
+
+        if (!multipartFile.isEmpty()) {
             String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
             category.setImage(fileName);
 
             Category savedCategory = service.save(category);
-            String uploadDir = "./category-images/" + savedCategory.getId();
+            String uploadDir = "../category-images/" + savedCategory.getId();
+
             FileUploadUtil.cleanDir(uploadDir);
             FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
         } else {
             service.save(category);
         }
-        ra.addFlashAttribute("message", "Thêm danh mục thành công!");
+
+        redirectAttributes.addFlashAttribute("message", "Lưu danh mục thành công");
         return "redirect:/categories";
     }
     @GetMapping("/categories/edit/{id}")
@@ -55,10 +63,9 @@ public class CategoryController {
         try {
             Category category = service.get(id);
             List<Category> listCategories = service.listCategoriesUsedInForm();
-
             model.addAttribute("category", category);
             model.addAttribute("listCategories", listCategories);
-            model.addAttribute("pageTitle", "Edit Category (ID: " + id + ")");
+            model.addAttribute("pageTitle", "Sửa danh mục (ID: " + id + ")");
 
             return "categories/category_form";
         } catch (CategoryNotFoundException ex) {
--- ShoptechProject/ShoptechWebParent/ShoptechBackEnd/src/main/java/com/shoptech/admin/category/CategoryService.java
+++ ShoptechProject/ShoptechWebParent/ShoptechBackEnd/src/main/java/com/shoptech/admin/category/CategoryService.java
@@ -2,41 +2,48 @@ package com.shoptech.admin.category;
 
 import com.shoptech.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Set;
+import java.util.*;
 
 @Service
 public class CategoryService {
     @Autowired
     private CategoryRepository repo;
-    public List<Category> listAll(){
-        List<Category> rootCategories = repo.findRootCategories();
-        return listHierarchicalCategories(rootCategories);
+    public List<Category> listAll(String sortDir){
+        Sort sort = Sort.by("name");
+
+        if (sortDir == null || sortDir.isEmpty()){
+            sort = sort.ascending();
+        } else if(sortDir.equals("asc")){
+            sort = sort.ascending();
+        } else if (sortDir.equals("desc")){
+            sort = sort.descending();
+        }
+        List<Category> rootCategories = repo.findRootCategories(sort);
+        return listHierarchicalCategories(rootCategories, sortDir);
     }
-    private List<Category> listHierarchicalCategories(List<Category> rootCategories) {
+    private List<Category> listHierarchicalCategories(List<Category> rootCategories, String sortDir) {
         List<Category> hierarchicalCategories = new ArrayList<>();
         for (Category rootCategory : rootCategories){
             hierarchicalCategories.add(Category.copyFull(rootCategory));
 
-            Set<Category> children = rootCategory.getChildren();
+            Set<Category> children = sortSubCategories(rootCategory.getChildren(), sortDir);
             for (Category subCategory : children) {
                 String name = "--" + subCategory.getName();
                 hierarchicalCategories.add(Category.copyFull(subCategory, name));
 
-                listSubHierarchicalCategories(hierarchicalCategories,subCategory,1);
+                listSubHierarchicalCategories(hierarchicalCategories,subCategory,1, sortDir);
             }
         }
 
         return hierarchicalCategories;
     }
     private void listSubHierarchicalCategories(List<Category> hierarchicalCategories,
-                                               Category parent, int subLevel) {
+                                               Category parent, int subLevel, String sortDir) {
         int newSubLevel = subLevel + 1;
-        Set<Category> children = parent.getChildren();
+        Set<Category> children = sortSubCategories(parent.getChildren(), sortDir);
 
         for (Category subCategory : children){
             String name = "";
@@ -46,7 +53,7 @@ public class CategoryService {
             name += subCategory.getName();
             hierarchicalCategories.add(Category.copyFull(subCategory, name));
 
-            listSubHierarchicalCategories(hierarchicalCategories, subCategory, newSubLevel);
+            listSubHierarchicalCategories(hierarchicalCategories, subCategory, newSubLevel, sortDir);
         }
     }
     public Category save(Category category) {
@@ -54,12 +61,12 @@ public class CategoryService {
     }
     public List<Category> listCategoriesUsedInForm(){
         List<Category> categoriesUsedInForm = new ArrayList<>();
-        Iterable<Category> categoriesInDB = repo.findAll();
+        Iterable<Category> categoriesInDB = repo.findRootCategories(Sort.by("name").ascending());
 
         for (Category category : categoriesInDB){
             if (category.getParent() == null) {
                 categoriesUsedInForm.add(Category.copyIdAndName(category));
-                Set<Category> children = category.getChildren();
+                Set<Category> children = sortSubCategories(category.getChildren());
                 for (Category subCategory : children){
                     String name = "--" + subCategory.getName();
                     categoriesUsedInForm.add(Category.copyIdAndName(subCategory.getId(), name));
@@ -71,7 +78,7 @@ public class CategoryService {
     }
     private void listSubCategoriesUsedInForm(List<Category> categoriesUsedInForm, Category parent, int sublevel) {
         int newSubLevel = sublevel + 1;
-        Set<Category> children = parent.getChildren();
+        Set<Category> children = sortSubCategories(parent.getChildren());
         for (Category subCategory : children){
             String name = "";
             for (int i = 0; i < newSubLevel; i++) {
@@ -88,5 +95,60 @@ public class CategoryService {
         } catch (NoSuchElementException ex){
             throw new CategoryNotFoundException("Không tìm thấy danh mục với ID = " + id);
         }
+    }
+    public String checkUnique(Integer id, String name, String alias) {
+
+        boolean isCreatingNew = (id == null || id == 0);
+
+        Category categoryByName = repo.findByName(name);
+
+        // create new user
+        if (isCreatingNew) {
+            if (categoryByName != null) {
+                return "DuplicateName";
+            } else {
+                Category categoryByAlias = repo.findByAlias(alias);
+                if (categoryByAlias != null) {
+                    return "DuplicateAlias";
+                }
+            }
+        } else {
+            // edit existing user
+            if (categoryByName != null && categoryByName.getId() != id) {
+                return "DuplicateName";
+            }
+
+            Category categoryByAlias = repo.findByAlias(alias);
+            if (categoryByAlias != null && categoryByAlias.getId() != id) {
+                return "DuplicateAlias";
+            }
+
+        }
+
+        return "OK";
+
+    }
+    private SortedSet<Category> sortSubCategories(Set<Category> children) {
+        return sortSubCategories(children, "asc");
+    }
+
+    private SortedSet<Category> sortSubCategories(Set<Category> children, String sortDir) {
+        SortedSet<Category> sortedChildren = new TreeSet<>(new Comparator<Category>() {
+
+            @Override
+            public int compare(Category cat1, Category cat2) {
+
+                if (sortDir.equals("asc")) {
+                    return cat1.getName().compareTo(cat2.getName());
+                } else {
+                    return cat2.getName().compareTo(cat1.getName());
+                }
+            }
+
+        });
+
+        sortedChildren.addAll(children);
+
+        return sortedChildren;
     }
 }
