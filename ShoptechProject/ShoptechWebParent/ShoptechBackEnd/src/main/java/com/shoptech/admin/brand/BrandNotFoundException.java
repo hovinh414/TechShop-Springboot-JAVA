b2404b97f8da822c8e9d30e9ef909f0845ea0850
hovinh414 <hovinh414@gmail.com>
2023-05-22 02:21:45 +0700

    Update list, create, update, delete brand

---
+++ ShoptechProject/ShoptechWebParent/ShoptechBackEnd/src/main/java/com/shoptech/admin/brand/BrandNotFoundException.java
@@ -0,0 +1,9 @@
+package com.shoptech.admin.brand;
+
+public class BrandNotFoundException extends Exception {
+
+    public BrandNotFoundException(String message) {
+        super(message);
+    }
+
+}
--- ShoptechProject/ShoptechWebParent/ShoptechBackEnd/src/main/java/com/shoptech/admin/brand/BrandRepository.java
+++ ShoptechProject/ShoptechWebParent/ShoptechBackEnd/src/main/java/com/shoptech/admin/brand/BrandRepository.java
@@ -5,6 +5,7 @@ import com.shoptech.entity.Brand;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BrandRepository extends CrudRepository<Brand, Integer> {
-
+    public Long countById(Integer id);
+    public Brand findByName(String name);
 }
 
