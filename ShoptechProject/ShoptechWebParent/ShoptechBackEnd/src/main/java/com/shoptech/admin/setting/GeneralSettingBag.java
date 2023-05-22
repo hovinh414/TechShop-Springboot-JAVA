c65dc5c1c649717ca4a73e477c460e9a7985fec8
VeNss <78064768+VeiNss1403@users.noreply.github.com>
2023-05-23 00:17:25 +0700

    update settings

---
+++ ShoptechProject/ShoptechWebParent/ShoptechBackEnd/src/main/java/com/shoptech/admin/setting/GeneralSettingBag.java
@@ -0,0 +1,21 @@
+package com.shoptech.admin.setting;
+
+import java.util.List;
+
+import com.shoptech.entity.Setting;
+import com.shoptech.entity.SettingBag;
+
+public class GeneralSettingBag extends SettingBag {
+
+	public GeneralSettingBag(List<Setting> listSettings) {
+		super(listSettings);
+	}
+
+	public void updateCurrencySymbol(String value) {
+		super.update("CURRENCY_SYMBOL", value);
+	}
+	
+	public void updateSiteLogo(String value) {
+		super.update("SITE_LOGO", value);
+	}
+}
