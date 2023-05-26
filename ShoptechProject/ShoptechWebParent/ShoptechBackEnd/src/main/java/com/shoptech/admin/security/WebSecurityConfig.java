c8314fd31a1bc3305868d609c570358b2aeb5eb3
PhanHongYenQuynh <luisywang1411@gmail.com>
2023-05-26 19:04:40 +0700

    Update Remember Me by Quynh

--- ShoptechProject/ShoptechWebParent/ShoptechBackEnd/src/main/java/com/shoptech/admin/security/WebSecurityConfig.java
+++ ShoptechProject/ShoptechWebParent/ShoptechBackEnd/src/main/java/com/shoptech/admin/security/WebSecurityConfig.java
@@ -3,11 +3,9 @@ package com.shoptech.admin.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -35,13 +33,7 @@ public class WebSecurityConfig {
         return authProvider;
     }
 
-<<<<<<< HEAD
-    //NOTE: Đóng từ dòng 39 đến 87  [đang lỗi chưa bik fix]
-    /*@Bean
-=======
-
     @Bean
->>>>>>> main
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
                 .requestMatchers("/users/**", "/settings/**", "/countries/**", "/states/**").hasAuthority("Admin")
@@ -64,45 +56,32 @@ public class WebSecurityConfig {
                 .formLogin()
                 .loginPage("/login")
                 .usernameParameter("email")
+                .defaultSuccessUrl("/home")
                 .permitAll()
-                .and().logout().permitAll();
-                *//*.and().rememberMe().key("AbcDefgHijKlmnOpqrs_1234567890")
-                .tokenValiditySeconds(7 * 24 * 60 * 60);  // remember me cookie is valid for one week*//*
+                .and().logout().permitAll()
+                .and().rememberMe()
+                .key("AbcDefgHijKlmnOpqrs_1234567890")
+                .tokenValiditySeconds(7 * 24 * 60 * 60)
+                .userDetailsService(userDetailsService());
 
-<<<<<<< HEAD
-        *//*
-         * set a fix key used for the md5 hash algorithm to encrypt cookie content each
-         * time the application is restarted, a new key will be generated; this key will
-         * be used to encrypt the cookie content so, in order for the cookie to be
-         * permanent; a fix key is needed.
-         *//*
-
-=======
->>>>>>> main
         http.authenticationProvider(authenticationProvider());
-
         return http.build();
     }
+
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/css/**");
-    }*/
+    }
 
 
-<<<<<<< HEAD
     //NOTE: Mở Cái này là vô được admin
 
-    @Bean
-=======
-    /*@Bean
->>>>>>> main
+   /* @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
         http.authorizeRequests().anyRequest().permitAll();
         return http.build();
-    }
-
-
+    }*/
 
 }
