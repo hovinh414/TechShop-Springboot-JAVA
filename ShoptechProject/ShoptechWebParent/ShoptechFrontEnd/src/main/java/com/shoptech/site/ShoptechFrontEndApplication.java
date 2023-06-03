b782f703bafc63df5feb534a6ee4b2d8cde49f62
nguyentrungne <114131139+nguyentrungne@users.noreply.github.com>
2023-06-03 12:04:05 +0700

    Update login and account update by Trung

--- ShoptechProject/ShoptechWebParent/ShoptechFrontEnd/src/main/java/com/shoptech/site/ShoptechFrontEndApplication.java
+++ ShoptechProject/ShoptechWebParent/ShoptechFrontEnd/src/main/java/com/shoptech/site/ShoptechFrontEndApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShoptechFrontEndApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("server.servlet.context-path", "/tech");
 		SpringApplication.run(ShoptechFrontEndApplication.class, args);
 	}
 
