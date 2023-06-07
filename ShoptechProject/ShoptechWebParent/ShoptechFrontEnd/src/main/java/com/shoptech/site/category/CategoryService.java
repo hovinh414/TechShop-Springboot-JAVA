1add2f194c93f8acfdc7e308bf12d8a9165ce010
hovinh414 <hovinh414@gmail.com>
2023-06-07 14:58:41 +0700

    update list product

--- ShoptechProject/ShoptechWebParent/ShoptechFrontEnd/src/main/java/com/shoptech/site/category/CategoryService.java
+++ ShoptechProject/ShoptechWebParent/ShoptechFrontEnd/src/main/java/com/shoptech/site/category/CategoryService.java
@@ -5,11 +5,11 @@ import com.shoptech.exception.CategoryNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+
 @EntityScan("com.shoptech.entity")
 @Service
 @Transactional
@@ -62,7 +62,7 @@ public class CategoryService {
     public Category getCategory(String alias) throws CategoryNotFoundException {
          Category category=repo.findByAliasEnabled(alias);
          if (category == null){
-             throw new CategoryNotFoundException("Không thể tìm thấy danh mục" + alias);
+             throw new CategoryNotFoundException("Could not file category with alias" + alias);
          }
          return category;
     }
@@ -80,5 +80,71 @@ public class CategoryService {
         listParents.add(child);
 
         return listParents;
+    }
+    private SortedSet<Category> sortSubCategories(Set<Category> children) {
+        return sortSubCategories(children, "asc");
+    }
+    private SortedSet<Category> sortSubCategories(Set<Category> children, String sortDir) {
+        SortedSet<Category> sortedChildren = new TreeSet<>(new Comparator<Category>() {
+
+            @Override
+            public int compare(Category cat1, Category cat2) {
+
+                if (sortDir.equals("asc")) {
+                    return cat1.getName().compareTo(cat2.getName());
+                } else {
+                    return cat2.getName().compareTo(cat1.getName());
+                }
+            }
+
+        });
+
+        sortedChildren.addAll(children);
+
+        return sortedChildren;
+    }
+    public List<Category> listCategoriesUsedInForm() {
+
+        List<Category> categoriesUsedInForm = new ArrayList<>();
+
+        Iterable<Category> categoriesInDB = repo.findRootCategories(Sort.by("name").ascending());
+
+        for (Category category : categoriesInDB) {
+
+            // root category
+            if (category.getParent() == null) {
+                categoriesUsedInForm.add(Category.copyIdAndName(category));
+
+                sortSubCategories(category.getChildren()).forEach(subCategory -> {
+                    String name = "" + subCategory.getName();
+
+                    categoriesUsedInForm.add(Category.copyIdAndNameAndAlias(subCategory.getId(), name, subCategory.getAlias()));
+                    listSubCategoriesUsedInForm(categoriesUsedInForm, subCategory, 1);
+                });
+            }
+
+        }
+
+        return categoriesUsedInForm;
+    }
+
+    // recursive method
+    private void listSubCategoriesUsedInForm(List<Category> categoriesUsedInForm, Category parent, int subLevel) {
+
+        int newSubLevel = subLevel + 1;
+        Set<Category> children = sortSubCategories(parent.getChildren());
+
+        for (Category subCategory : children) {
+
+            String name = "";
+            for (int i = 0; i < newSubLevel; i++) {
+                name += "";
+            }
+            name += subCategory.getName();
+            categoriesUsedInForm.add(Category.copyIdAndNameAndAlias(subCategory.getId(), name, subCategory.getAlias()));
+
+            listSubCategoriesUsedInForm(categoriesUsedInForm, subCategory, newSubLevel);
+        }
+
     }
 }
